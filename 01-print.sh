cbc68e4dd1b28c1aa00f27dd932ea37413d7757e
KalluruS <>
2021-04-15 19:47:38 +0530

    Update shell-scripting/

--- 01-print.sh
+++ 01-print.sh
@@ -12,3 +12,21 @@ echo Bye
 
 echo -e "Hai1\nBye1"
 
+# \n is used to print a new line
+
+# Some times we need colors to be printed with the text.
+
+## Following are the colors that are supported
+
+#   Color               Code
+# -------------------------------------
+#   Red                   31
+#   Green                 32
+#   Yellow                33
+#   Blue                  34
+#   Magenta               35
+#   Cyan                  36
+
+# Syntax: echo -e "\e[COLmMESSAGE"
+
+echo -e "\e[31mWelcome to DevOps Training"
