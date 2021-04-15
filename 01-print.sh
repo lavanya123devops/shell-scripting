d890f86831ec7cf2d2c1f40e27061e3c93bdb09f
KalluruS <>
2021-04-15 19:43:26 +0530

    Update shell-scripting/

--- 01-print.sh
+++ 01-print.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo Hello World
+
+## Multi Line Output
+
+echo Hai
+echo Bye
+
+# Single echo command but multi line output
+# Syntax: echo -e "Line1\nLine2"
+
+echo -e "Hai1\nBye1"
+
