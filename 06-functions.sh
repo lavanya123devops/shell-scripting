98d1cc0e81d4588d43c4c4fa5b88d7b3b23d0bb7
KalluruS <>
2021-04-16 19:14:59 +0530

    Update shell-scripting/

---
+++ 06-functions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Declare the function
+function sample1() {
+    echo Hello from Function1
+}
+
+sample2() {
+  echo Hello from Funtion2
+}
+
+## Access the function
+
+sample1
+sample2
+
