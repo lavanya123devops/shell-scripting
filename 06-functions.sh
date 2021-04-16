d89c971c25301aa08f7e5ef5981d8c80578fd45e
KalluruS <>
2021-04-16 19:20:09 +0530

    Update shell-scripting/

--- 06-functions.sh
+++ 06-functions.sh
@@ -9,6 +9,8 @@ function sample1() {
 
 sample2() {
   echo Hello from Funtion2
+
+  echo First Argument in Function = $1
 }
 
 ## Access the function
@@ -17,4 +19,8 @@ a=10
 sample1
 sample2
 echo b - $b
+
+
+echo First Argument = $1
+
 
