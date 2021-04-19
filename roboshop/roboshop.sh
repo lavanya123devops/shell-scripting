3d8e3ae048ee5c07221062aec1a1bd806393db26
KalluruS <>
2021-04-19 18:46:51 +0530

    Update shell-scripting/

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -2,17 +2,19 @@
 
 COMPONENT=$1
 
+source components/common.sh
+
 if [ ! -f components/${COMPONENT}.sh ]; then
-  echo "Invalid Component Name is Provided"
+  ERROR "Invalid Component Name is Provided"
   exit 1
 fi
 
 USER_NAME=$(whoami)
 
 if [ "${USER_NAME}" != "root" ]; then
-  echo "You should be a root user to perform this script, Try with sudo"
+  ERROR "You should be a root user to perform this script, Try with sudo"
   exit 1
-fi 
+fi
 
 export COMPONENT
 bash components/${COMPONENT}.sh
