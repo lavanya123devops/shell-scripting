1f07cabbe54fe3f6d571c629b48938e551aeffa8
KalluruS <>
2021-04-19 18:45:07 +0530

    Update shell-scripting/

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -7,5 +7,12 @@ if [ ! -f components/${COMPONENT}.sh ]; then
   exit 1
 fi
 
+USER_NAME=$(whoami)
+
+if [ "${USER_NAME}" != "root" ]; then
+  echo "You should be a root user to perform this script, Try with sudo"
+  exit 1
+fi 
+
 export COMPONENT
 bash components/${COMPONENT}.sh
