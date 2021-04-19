dfcc0141c1162ed42c63e096eb61b01b64cfe807
KalluruS <>
2021-04-19 20:02:37 +0530

    Update shell-scripting/

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -9,14 +9,18 @@ apt install npm -y &>>$LOG
 Stat $?
 
 Head "Adding RoboShop User"
-useradd -m -s /bin/bash roboshop
-Stat $?
+id roboshop &>>$LOG
+if [ $? -ne 0 ]; then
+  useradd -m -s /bin/bash roboshop
+  Stat $?
+fi
+
 
 
 DOWNLOAD_COMPONENT
 
 Head "Extracting Downloaded Archive"
-cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install &>>$LOG && chown roboshop:roboshop /home/roboshop -R 
+cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install &>>$LOG && chown roboshop:roboshop /home/roboshop -R
 Stat $?
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
