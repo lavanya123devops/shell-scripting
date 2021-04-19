9807c3c7ffa09d463b341e52000e84795569b9a0
KalluruS <>
2021-04-19 19:57:20 +0530

    Update shell-scripting/

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo Automating catalogue component
+source components/common.sh
+
+OS_PREREQ
+
+Head "Install NPM"
+apt install npm -y &>>$LOG
+Stat $?
+
+Head "Adding RoboShop User"
+useradd -m -s /bin/bash roboshop
+Stat $?
+
+
+DOWNLOAD_COMPONENT
+
+Head "Extracting Downloaded Archive"
+cd /home/roboshop && unzip -o /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install
+Stat $?
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
