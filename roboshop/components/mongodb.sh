509a4c62b3e8b5af6aa96091dbbcd422baa4cf65
KalluruS <>
2021-04-19 19:41:43 +0530

    Update shell-scripting/

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -29,7 +29,7 @@ Stat $?
 Head "Load Schema into MongoDB"
 for i in $(ls *.js); do
   echo "Loading $i"
-  mongo --shell --host 0.0.0.0
+  mongo --shell --host 0.0.0.0 --idleSessionTimeout 30
   Stat $?
 done
 
