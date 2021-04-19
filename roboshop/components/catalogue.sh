d26d2b18b1da49e239f977c7434174c67b9b0818
KalluruS <>
2021-04-19 20:07:34 +0530

    Update shell-scripting/

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -20,7 +20,7 @@ fi
 DOWNLOAD_COMPONENT
 
 Head "Extracting Downloaded Archive"
-cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install &>>$LOG && chown roboshop:roboshop /home/roboshop -R
+cd /home/roboshop && rm -rf catalogue && unzip -o /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install &>>$LOG && chown roboshop:roboshop /home/roboshop -R
 Stat $?
 
 Head "Update EndPoints in Service File"
