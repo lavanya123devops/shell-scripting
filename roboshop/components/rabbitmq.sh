24948452c85424d7519cfe80156101519a427b8c
KalluruS <>
2021-04-20 19:05:24 +0530

    Update shell-scripting/

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo Automating rabbitmq component
+source components/common.sh
+
+OS_PREREQ
+
+Head "Install RabbitMQ Repos"
+curl -fsSL https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc | apt-key add - &>>$LOG
+Stat $?
+
+Head "Setup RabbitMQ Sources"
+apt install apt-transport-https -y &>>$LOG && echo -e "deb https://dl.bintray.com/rabbitmq-erlang/debian focal erlang\ndeb https://dl.bintray.com/rabbitmq/debian bionic main" > /etc/apt/sources.list.d/bintray.rabbitmq.list && apt update -y &>>$LOG
+Stat $?
+
+Head "Install RabbitMQ"
+apt install rabbitmq-server -y --fix-missing &>>$LOG
+Stat $?
+
+Head "Setup Application User and Setup Permissions"
+rabbitmqctl add_user roboshop roboshop123 && rabbitmqctl set_user_tags roboshop administrator && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+Stat $?
+
