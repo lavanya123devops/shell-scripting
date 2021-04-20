10e308a19c751a6b343d26a3ca91cad4f88ebae2
KalluruS <>
2021-04-20 18:59:23 +0530

    Update shell-scripting/

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-echo Automating shipping component
+source components/common.sh
+
+OS_PREREQ
+
+Head "Install Maven"
+apt install maven -y &>>$LOG
+Stat $?
+
+
+Head "Adding RoboShop User"
+id roboshop &>>$LOG
+if [ $? -ne 0 ]; then
+  useradd -m -s /bin/bash roboshop
+  Stat $?
+fi
+
+DOWNLOAD_COMPONENT
+
+Head "Extract Downloaded Archive"
+cd /home/roboshop && rm -rf shipping && unzip -o /tmp/shipping.zip &>>$LOG && mv shipping-main shipping && cd /home/roboshop/shipping &&  mvn clean package  &>>$LOG && chown roboshop:roboshop /home/roboshop -R && mv target/shipping-1.0.jar shipping.jar  &>>$LOG
+Stat $?
+
+# mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
+# systemctl daemon-reload
+# systemctl start shipping
+# systemctl enable shipping
