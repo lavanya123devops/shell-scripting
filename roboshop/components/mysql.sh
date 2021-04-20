3f31a5154c5d0188c0cb3d04a63ed4a5503e276a
KalluruS <>
2021-04-20 18:44:49 +0530

    Update shell-scripting/

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-echo Automating mysql component
+source components/common.sh
+
+OS_PREREQ
+
+Head "Install Docker"
+curl -s https://get.docker.com | bash  &>>$LOG
+Stat $?
+
+Head "Start Docker"
+systemctl enable docker &>>$LOG  && systemctl start docker
+Stat $?
+
+Head "Start MySQL Service with Docker"
+mkdir -p /data
+docker run -v /data:/var/lib/mysql -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=password mysql:5.7.33 &>>$LOG
+Stat $?
+
+DOWNLOAD_COMPONENT
+
+Head "Extract Downloaded Schema"
+cd /tmp && unzip -o /tmp/mysql.zip &>>$LOG
+Stat $?
+
+Head "Load Schema"
+cd mysql-main
+IPADDR=$(hostname -i)
+mysql -h ${IPADDR} -uroot -ppassword <shipping.sql &>>$LOG
+STAT $?
