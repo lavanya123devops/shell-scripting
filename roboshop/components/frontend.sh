96f7be8c80c2a2d0b50c1441981d4be5b6c93442
KalluruS <>
2021-04-20 19:21:08 +0530

    Update shell-scripting/

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -20,6 +20,9 @@ Stat $?
 
 Head "Update Nginx Configuration"
 mv roboshop.conf /etc/nginx/sites-enabled/roboshop.conf
+for comp in catalogue cart user shipping payment ; do
+  sed -i -e "/$comp/ s/localhost/${comp}.zsdevops01.online/" /etc/nginx/sites-enabled/roboshop.conf
+done
 Stat $?
 
 Head "Restart Nginx Service"
